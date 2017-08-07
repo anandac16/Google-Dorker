18b68c880ab44cea978b442b160ce0caddfcf232
Anandac16 <achandrak16@gmail.com>
2017-08-07 10:32:41 +0700

    Update

--- x.php
+++ x.php
@@ -5,6 +5,12 @@ function url_check($url) {
     return is_array($headers) ? preg_match('/^HTTP\\/\\d+\\.\\d+\\s+2\\d\\d\\s+.*$/',$headers[0]) : false;
 };
 
+function save($file,$data){
+        $fp = @fopen($file, "a") or die("cant open file");
+        fwrite($fp, $data);
+        fclose($fp);
+}
+
   function get_links($url) {
 
         // Create a new DOM Document to hold our webpage structure
@@ -29,32 +35,34 @@ function url_check($url) {
         return $links;
     }
 
-$search = "sukasuka";
+$search = "amsoai";
 $searchs = urlencode($search);
 for($i=0;$i<=1000;$i+=10){
-$url = "https://www.google.com/search?q=$searchs&start=$i&ie=utf-8&oe=utf-8";
-$k = get_links($url);
-$l = json_encode($k);
+	$url = "https://www.google.com/search?q=$searchs&start=$i&ie=utf-8&oe=utf-8";
+	$k = get_links($url);
+	$l = json_encode($k);
 
-$z = explode('url?q=',$l);
-foreach($z as $o){
-if(preg_match('/googleusercontent/',$o)){
-	$x = explode('%25',$o);
-	$a = explode(':',$x[0]);
-	$urls = $a[2];
-}else{
-	$x = explode('&sa',$o);
-	$urls = $x[0];
-}
-$urls = str_replace('\/','/',$urls);
-$urls = urldecode($urls);
-if(url_check($urls)){
-print $urls;
-echo"\r\n";
-}else{
-print $urls;
-echo"\r\n";
-}
-}
+	$z = explode('url?q=',$l);
+	foreach($z as $o){
+		if(preg_match('/googleusercontent/',$o)){
+			$x = explode('%25',$o);
+			$a = explode(':',$x[0]);
+			$urls = $a[2];
+		}else{
+			$x = explode('&sa',$o);
+			$urls = $x[0];
+		}
+		$urls = str_replace('\/','/',$urls);
+		$urls = urldecode($urls);
+		if(url_check($urls)){
+			print $urls;
+			save("valid-urls.txt",$urls."\r\n");
+			echo"\r\n";
+		}else{
+			save("unknown-urls.txt",$urls."\r\n");
+			print $urls;
+			echo"\r\n";
+		}
+	}
 }
 ?>
